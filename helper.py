b923590a9b775ad99e862f92389f2272bba1d761
DANIEL <dfarrugia6@gmail.com>
2017-04-21 19:44:11 +1000

    Need to fix vowel map, not matching because stresses are still in pronounciation. Filter first.

--- helper.py
+++ helper.py
@@ -9,9 +9,12 @@ def read_data(file_path):
 		lines = f.read().splitlines()
 	return lines
 
-def stress_map(pronunciation):
-	pn_list = pronunciation.split()
-	return [1 if '1' in num else 0 for num in pn_list]
+def stress_map(pronunciation,stress='1'):
+	return [1 if stress in num else 0 for num in pronunciation]
+
+def phenom_map(pronunciation,phenom_list):
+	return [1 if phenom in phenom_list else 0 for phenom in pronunciation]
+
 
 class word(object):
 	"""docstring for word"""
@@ -19,8 +22,13 @@ class word(object):
 		super(word, self).__init__()
 		self.word = word_string.split(':')[0]
 		self.pronunciation = word_string.split(':')[1]
-		self.stress_map = stress_map(self.pronunciation)
+		self.pn_list = self.pronunciation.split()
+		self.primary_stress_map = stress_map(self.pn_list)
+		self.secondary_stress_map = stress_map(self.pn_list, stress='2')
+		self.vowel_map = phenom_map(self.pn_list,vowels)
+		self.consonant_map = phenom_map(self.pn_list,consonants)
 
 for line in read_data('asset/training_data.txt'):
 	x = word(line)
-	print(x.stress_map)
+	print(x.word)
+	print(x.consonant_map)	
