d5c4d0ba653dc09945a523fb4d10511539abbc5c
DANIEL <dfarrugia6@gmail.com>
2017-04-21 16:09:12 +1000

    Include Phenoms

--- helper.py
+++ helper.py
@@ -1,4 +1,14 @@
+# Phenoms
+vowels= ['AA','AE', 'AH', 'AO', 'AW', 'AY', 'EH', 'ER', 'EY', 'IH'
+, 'IY', 'OW', 'OY', 'UH', 'UW']
+consonants= ['P', 'B', 'CH', 'D', 'DH', 'F', 'G', 'HH', 'JH', 'K', 'L', 'M', 
+'N', 'NG', 'R', 'S', 'SH', 'T', 'TH', 'V', 'W', 'Y', 'Z', 'ZH']
+
+
 def read_data(file_path):
 	with open(file_path) as f:
 		lines = f.read().splitlines()
 	return lines
+
+
+
