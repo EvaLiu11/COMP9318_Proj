b2a4c313d9cdf1220f5bd00cfe45b00314a8842e
DANIEL <dfarrugia6@gmail.com>
2017-04-28 10:22:30 +1000

    Fixed vector map

--- helper.py
+++ helper.py
@@ -10,6 +10,10 @@ def read_data(file_path):
 		lines = f.read().splitlines()
 	return lines
 
+# Filter numbers from string
+def filter_stress(string):
+	return ''.join([i for i in string if not i.isdigit()])
+
 # Maps the location of the stress, 1 if stress at position
 # 0 otherwise
 def stress_map(pronunciation,stress='1'):
@@ -17,10 +21,13 @@ def stress_map(pronunciation,stress='1'):
 
 # Maps the the location of phenom, 1 in phenom_list
 # 0 otherwise
+# When being used to map phenoms to vector_map filters stresses filters stresses
+# first, this step is superfluous for for vowel and consonant map
 def phenom_map(pronunciation,phenom_list):
+	phenom_list = [filter_stress(phenom) for phenom in phenom_list]
 	return [1 if phenom in phenom_list else 0 for phenom in pronunciation]
 
-
+# Object to hold each word
 class word(object):
 	"""docstring for word"""
 	def __init__(self, word_string):
@@ -32,10 +39,9 @@ class word(object):
 		self.secondary_stress_map = stress_map(self.pn_list, stress='2')
 		self.vowel_map = phenom_map(self.pn_list,vowels)
 		self.consonant_map = phenom_map(self.pn_list,consonants)
-		self.vector_map = phenom_map(vector_map,filter(str.isalpha,self.pn_list))
+		self.vector_map = phenom_map(vector_map,self.pn_list)
 
 for line in read_data('asset/training_data.txt'):
 	x = word(line)
-	print(x.word)
-	print(x.pronunciation)
+	print(x.pn_list)
 	print(x.vector_map)
